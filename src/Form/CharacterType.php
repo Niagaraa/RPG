de4a0eb2a3091f2e653cfcf42a7df4724433fbd1
Nolwenn Sachet <sachet.nolwenn@orange.fr>
2020-06-23 15:48:35 +0200

    add character is functionnal

---
+++ src/Form/CharacterType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Character;
+use App\Entity\Forum;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CharacterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('forum', EntityType::class, [
+                'class' => Forum::class,
+                'choice_label' => 'name',
+                'expanded' => false,
+                'multiple' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Character::class,
+        ]);
+    }
+}
