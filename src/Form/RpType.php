790ee77a9488ecaedb20b834be93d08fb45d666f
Nolwenn Sachet <sachet.nolwenn@orange.fr>
2020-08-20 19:44:29 +0200

    form rp

--- src/Form/RpType.php
+++ src/Form/RpType.php
@@ -2,8 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Character;
 use App\Entity\Rp;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +17,34 @@ class RpType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('partner')
-            ->add('lastAnswer')
-            ->add('mustAnswering')
-            ->add('isEvent')
-            ->add('character')
-            ->add('forum')
+            ->add('character', EntityType::class, [
+                'class' => Character::class,
+                'choice_label' => 'name',
+                'label' => 'Personnage'
+            ])
+            ->add('title', TextType::class, [
+                'label' => 'Titre du RP'
+            ])
+            ->add('partner', TextType::class, [
+                'label' => 'Nom du partenaire'
+            ])
+            ->add('lastAnswer', DateType::class, [
+                'label' => 'Dernière réponse'
+            ])
+            ->add('mustAnswering', ChoiceType::class, [
+                'label' => 'Qui doit répondre ?',
+                'choices' => [
+                    'Moi' => 'Moi',
+                    'Mon partenaire' => 'Mon partenaire'
+                ]
+            ])
+            ->add('isEvent', ChoiceType::class, [
+                'label' => 'Event ou RP normal ?',
+                'choices' => [
+                    'Normal' => 'Normal',
+                    'Event' => 'Event'
+                ]
+            ])
         ;
     }
 
